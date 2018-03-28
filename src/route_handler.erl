253afc3693b19bcaa2537f393382d4a33bc4dda8
Katsuaki AWAIISHIMA <Katsuaki_Awaiishima@voyagegroup.com>
2018-03-28 17:05:47 +0900

    Fix: typo

--- src/route_handler.erl
+++ src/route_handler.erl
@@ -8,7 +8,6 @@ init(Req0, State) ->
     Req = cowboy_req:reply(200, #{
         <<"content-type">> => <<"text/plain">>
     }, <<"Hello">>, Req0),
-    })
     {ok, Req, State}.
 
 handle(Req, State, F) ->
